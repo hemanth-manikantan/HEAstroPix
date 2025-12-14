a33ffb53d810e83e33a93df4070dfa99190d05b3
hemanth-manikantan <hemmotog@gmail.com>
2025-12-14 19:06:43 +0100

    First commit

--- hypex_hw.sh
+++ hypex_hw.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 PYTHON=/home/hemanthm/miniconda3/envs/hypex2/bin/python
-exec $PYTHON -m streamlit run /home/hemanthm/Analysis_HEA/Timepix3_2025/GridPix_Dec2025/UITool/app.py
+exec $PYTHON -m streamlit run /home/hemanthm/Analysis_HEA/Timepix3_2025/GridPix_Dec2025/HEAstroPix/app.py
